c78d9b4b22c96810e48d47f6a4fc1cd507d62c78
Charlie Castro <ccspart2@gmail.com>
2020-01-16 11:16:22 -0500

    Initial Commit

---
+++ app/src/main/java/com/ccsecurityservices/projectcerberusadmin/HomePage.kt
@@ -0,0 +1,12 @@
+package com.ccsecurityservices.projectcerberusadmin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HomePage : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.home_page)
+    }
+}
