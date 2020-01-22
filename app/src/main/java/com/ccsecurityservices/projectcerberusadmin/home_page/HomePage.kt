ba1df8251217e12ff38d1c0278418beee17e90a9
Charlie Castro <ccspart2@gmail.com>
2020-01-22 14:20:11 -0500

    implementation Kotlin synthetics for ImageView Binds

--- app/src/main/java/com/ccsecurityservices/projectcerberusadmin/home_page/HomePage.kt
+++ app/src/main/java/com/ccsecurityservices/projectcerberusadmin/home_page/HomePage.kt
@@ -1,15 +1,31 @@
 package com.ccsecurityservices.projectcerberusadmin.home_page
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.ccsecurityservices.projectcerberusadmin.Data_Items.User
 import com.ccsecurityservices.projectcerberusadmin.R
 
 class HomePage : AppCompatActivity(), HomePageContract.HomePageView {
 
+    private lateinit var employeesBTN: ImageView
+
+    private lateinit var homePagePresenter: HomePagePresenter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.home_page)
+
+        employeesBTN = findViewById(R.id.employees_btn)
+
+        employeesBTN.setOnClickListener{
+            Toast.makeText(this, "Funciona", Toast.LENGTH_LONG).show()
+        }
+
+
+
+
     }
 
     override fun populateUser(): User {
