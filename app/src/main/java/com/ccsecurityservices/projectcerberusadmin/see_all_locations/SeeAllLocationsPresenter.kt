e76ce8f4d303ffb8eaf9106ec5b72b0ede45445c
Charlie Castro <ccspart2@gmail.com>
2020-01-30 14:55:46 -0500

    Implementation of the SeeAllLocations Layout

--- app/src/main/java/com/ccsecurityservices/projectcerberusadmin/see_all_locations/SeeAllLocationsPresenter.kt
+++ app/src/main/java/com/ccsecurityservices/projectcerberusadmin/see_all_locations/SeeAllLocationsPresenter.kt
@@ -2,7 +2,7 @@ package com.ccsecurityservices.projectcerberusadmin.see_all_locations
 
 class SeeAllLocationsPresenter(private val view: SeeAllLocationsView) :
     SeeAllLocationsContract.SeeAllLocationsPresenter {
-    
+
     override fun getLocationList() {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
