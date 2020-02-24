3721aae5f23c2823d957ccd038c730e782fcf472
Charlie Castro <ccspart2@gmail.com>
2020-02-24 18:58:16 -0500

    Initial implementation for the Authentication.

--- app/src/main/java/com/ccsecurityservices/projectcerberusadmin/home_page/HomePagePresenter.kt
+++ app/src/main/java/com/ccsecurityservices/projectcerberusadmin/home_page/HomePagePresenter.kt
@@ -1,8 +1,28 @@
 package com.ccsecurityservices.projectcerberusadmin.home_page
 
-class HomePagePresenter(private val view: HomePageContract.HomePageView) : HomePageContract.HomePagePresenter{
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
+import com.google.firebase.auth.FirebaseAuth
+
+class HomePagePresenter(private val view: HomePageContract.HomePageView) :
+    HomePageContract.HomePagePresenter {
 
     override fun retrieveUserInfo() {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun checkIfLoggedIn() {
+        val auth = FirebaseAuth.getInstance()
+        if (auth.currentUser != null) {
+
+        } else {
+            view.startLogInFlow()
+        }
+    }
+
+    override fun checkIfValidAdmin(response: IdpResponse) {
+
+        val auth = response.email
+
     }
 }
--- app/src/main/java/com/ccsecurityservices/projectcerberusadmin/home_page/HomePageView.kt
+++ app/src/main/java/com/ccsecurityservices/projectcerberusadmin/home_page/HomePageView.kt
@@ -1,17 +1,25 @@
 package com.ccsecurityservices.projectcerberusadmin.home_page
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.ccsecurityservices.projectcerberusadmin.R
-import com.ccsecurityservices.projectcerberusadmin.add_new_event.AddNewEventView
 import com.ccsecurityservices.projectcerberusadmin.see_all_employees.SeeAllEmployeesView
 import com.ccsecurityservices.projectcerberusadmin.data_items.Employee
 import com.ccsecurityservices.projectcerberusadmin.see_all_events.SeeAllEventsView
 import com.ccsecurityservices.projectcerberusadmin.see_all_locations.SeeAllLocationsView
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.home_page.*
 
 class HomePageView : AppCompatActivity(), HomePageContract.HomePageView {
+
+    companion object {
+
+        private const val RC_SIGN_IN = 123
+    }
 
     private lateinit var homePagePresenter: HomePagePresenter
 
@@ -30,6 +38,19 @@ class HomePageView : AppCompatActivity(), HomePageContract.HomePageView {
         home_page_events_btn.setOnClickListener {
             navigateOutHomePage(it.id)
         }
+        home_page_log_out_BTN.setOnClickListener {
+            logOut()
+        }
+
+        homePagePresenter.checkIfLoggedIn()
+    }
+
+    private fun logOut() {
+        AuthUI.getInstance()
+            .signOut(this)
+            .addOnCompleteListener {
+                startLogInFlow()
+            }
     }
 
     private fun navigateOutHomePage(viewID: Int) {
@@ -51,5 +72,43 @@ class HomePageView : AppCompatActivity(), HomePageContract.HomePageView {
 
     override fun populateUser(): Employee {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun startLogInFlow() {
+        // Choose authentication providers
+        val providers = arrayListOf(
+            AuthUI.IdpConfig.EmailBuilder().build(),
+            AuthUI.IdpConfig.PhoneBuilder().build()
+        )
+
+        // Create and launch sign-in intent
+        startActivityForResult(
+            AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(providers)
+                .setLogo(R.drawable.cerberus_logo_color)
+                .build(),
+            RC_SIGN_IN
+        )
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RC_SIGN_IN) {
+            val response = IdpResponse.fromResultIntent(data)
+
+            if (resultCode == Activity.RESULT_OK) {
+                // Successfully signed in
+                val user = FirebaseAuth.getInstance().currentUser
+                homePagePresenter.checkIfValidAdmin(response!!)
+                // ...
+            } else {
+                // Sign in failed. If response is null the user canceled the
+                // sign-in flow using the back button. Otherwise check
+                // response.getError().getErrorCode() and handle the error.
+                // ...
+            }
+        }
     }
 }
