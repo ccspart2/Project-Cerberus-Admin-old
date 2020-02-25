4436976058a77380207ec0282fb02db5d7681d8c
Charlie Castro <ccspart2@gmail.com>
2020-02-25 08:10:46 -0500

    Decided to create a New Log In Activity

--- app/src/main/java/com/ccsecurityservices/projectcerberusadmin/home_page/HomePageContract.kt
+++ app/src/main/java/com/ccsecurityservices/projectcerberusadmin/home_page/HomePageContract.kt
@@ -1,18 +1,14 @@
 package com.ccsecurityservices.projectcerberusadmin.home_page
 
 import com.ccsecurityservices.projectcerberusadmin.data_items.Employee
-import com.firebase.ui.auth.IdpResponse
 
 interface HomePageContract {
 
     interface HomePageView {
         fun populateUser(): Employee
-        fun startLogInFlow()
     }
 
     interface HomePagePresenter {
         fun retrieveUserInfo()
-        fun checkIfLoggedIn()
-        fun checkIfValidAdmin(response: IdpResponse)
     }
 }
--- app/src/main/java/com/ccsecurityservices/projectcerberusadmin/home_page/HomePagePresenter.kt
+++ app/src/main/java/com/ccsecurityservices/projectcerberusadmin/home_page/HomePagePresenter.kt
@@ -1,28 +1,8 @@
 package com.ccsecurityservices.projectcerberusadmin.home_page
 
-import com.firebase.ui.auth.AuthUI
-import com.firebase.ui.auth.IdpResponse
-import com.google.firebase.auth.FirebaseAuth
-
-class HomePagePresenter(private val view: HomePageContract.HomePageView) :
-    HomePageContract.HomePagePresenter {
+class HomePagePresenter(private val view: HomePageContract.HomePageView) : HomePageContract.HomePagePresenter{
 
     override fun retrieveUserInfo() {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun checkIfLoggedIn() {
-        val auth = FirebaseAuth.getInstance()
-        if (auth.currentUser != null) {
-
-        } else {
-            view.startLogInFlow()
-        }
-    }
-
-    override fun checkIfValidAdmin(response: IdpResponse) {
-
-        val auth = response.email
-
     }
 }
